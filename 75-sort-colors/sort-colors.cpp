41f40fe7e954fb2b7aa31ca450d7066e370451ec
Atinshay Awasthi <157672307+AtinshayAwasthi@users.noreply.github.com>
2025-08-15 11:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.84%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //better approach
-        int c0=0,c1=0,c2=0;
-        for(int i=0;i<nums.size();i++)
+        //optimal approach(datch national flag algo)
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high)
         {
-            if(nums[i]==0) c0++;
-            else if(nums[i]==1) c1++;
-            else c2++;
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1) mid++;
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+
         }
-        
-            for(int i=0;i<c0;i++)
-            {
-                nums[i]=0;
-            }
-            for(int i=c0;i<c0+c1;i++)
-            {
-                nums[i]=1;
-            }
-            for(int i=c0+c1;i<nums.size();i++)
-            {
-                nums[i]=2;
-            }
     }
 };
